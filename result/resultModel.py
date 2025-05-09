e672d778f6ab3403ad12a6eb2b2acaddd9215fd0
Fern Nimneim <pontip_solo@hotmail.com>
2025-05-09 10:26:44 +0700

    Delete result directory

--- result/resultModel.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic import BaseModel
-class VPDAlarm(BaseModel):
-    MachineNo: str
-    MachineModel: str
-    Operation: str
-    DeviceID: str
-    AlarmDetail: str
-    # CREATEDATE :str
