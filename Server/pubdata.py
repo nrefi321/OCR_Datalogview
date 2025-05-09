8077bcae5b3ca829d08a5f2c11f972a12333fb91
Fern Nimneim <pontip_solo@hotmail.com>
2025-05-09 10:11:08 +0700

    Update pubdata.py

--- Server/pubdata.py
+++ Server/pubdata.py
@@ -1,10 +1,9 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt 
 import json
 import time
 
 a = 1 #round pub
 broker_address="127.0.0.1"
-#broker_address="pca571.nseb.co.th"
 MQTT_TOPIC = "vpd_all/midnight"
 MQTT_MSG=json.dumps({"time":"midnight"})
 
